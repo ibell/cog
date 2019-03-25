a054b0d1c2bcd8867ec92696c5c8286c4bd07e46
Ian Bell <ian.h.bell@gmail.com>
2019-03-24 19:50:22 -0600

    Now store the script and run it

--- experiments/sandboxed/sandboxed.py
+++ experiments/sandboxed/sandboxed.py
@@ -3,7 +3,10 @@ import timeit
 import docker
 import os
 import sys
-from io import StringIO
+import time
+from io import StringIO, BytesIO
+import tarfile
+import uuid
 
 # Flask-y things
 from flask import Flask, session
@@ -16,29 +19,49 @@ app.secret_key = os.urandom(40)
 
 client = docker.from_env()
 
-def make_instance(session):
+def string_to_container(container, s, dest_path):
+    pw_tarstream = BytesIO()
+
+    pw_tar = tarfile.TarFile(fileobj=pw_tarstream, mode='w')
+
+    file_data = s.encode('utf-8')
+    path = str(uuid.uuid1()).replace('-','').lower() + '.py'
+    tarinfo = tarfile.TarInfo(name=path)
+    tarinfo.size = len(s)
+    tarinfo.mtime = time.time()
+
+    pw_tar.addfile(tarinfo, BytesIO(file_data))
+    pw_tar.close()
+
+    pw_tarstream.seek(0)
+
+    success = container.put_archive(dest_path, pw_tarstream)
+    if not success:
+        print('uhoh')
+    else:
+        print('seemed to be ok')
+    return dest_path+path
+
+def get_instance(session):
     if 'docker_instance' not in session:        
         cont = client.containers.run("cog_worker", detach=True, remove=True, stdin_open=True, tty=True)
         session['docker_instance'] = cont.id
+    return client.containers.get(session['docker_instance'])
 
 @app.route('/', methods=['GET'])
 def safe_eval():
     tic = timeit.default_timer()
-    make_instance(session)
+    con = get_instance(session)
     toc = timeit.default_timer()
     print(toc-tic, 's to get instance')
-
-    con = client.containers.get(session['docker_instance'])
-    tic = timeit.default_timer()
-
-    def escape(code):
-        return code.replace('"', '\\"')
+    
     tic = timeit.default_timer()
     code = 'print("Hi from python");\nprint("Hello from python");'
-    cmd = 'python -c "' + escape(code) + '"'
-    exitcode, out = con.exec_run(cmd=cmd)
+    path = string_to_container(con, s=code, dest_path='/')
+    toc1 = timeit.default_timer()
+    exitcode, out = con.exec_run(cmd = 'python ' + path)
     toc = timeit.default_timer()
-    print(toc-tic, 's to call')
+    print(toc-tic, 's to call', toc1-tic)
     return out.decode('utf-8').replace('\n','<br>')
 
 if __name__ == '__main__':
